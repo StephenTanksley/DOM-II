0dea428993bdcee6bec137ce1fcd700175d2ca33
Stephen Tanksley <stephen.tanksley@gmail.com>
2019-08-12 15:04:47 -0500

    Got the event listener for the scroll event working. Moving on to mouseover and mouseout.

--- js/index.js
+++ js/index.js
@@ -6,12 +6,12 @@
 //function
 //listener + function
 
-// const logoHeading = document.getElementsByClassName('logo-heading');
+const logoHeading = document.querySelector('.logo-heading');
 
-// function scaleUp(e) {
-//     logoHeading.classList.toggle('large');
-// };
-// logoHeading.addEventListener('dblclick', scaleUp);
+function scaleUp(e) {
+    logoHeading.classList.toggle('large');
+};
+logoHeading.addEventListener('dblclick', scaleUp);
 
 // 2. select
 
@@ -20,7 +20,7 @@
 //listener + function
 
 function logSelection(e) { // creating an function that takes in an event, the 'select' event.
-    const log = document.getElementById('log') //creates a variable for the log of what we're going to capture.
+    const log = document.querySelector('#log') //creates a variable for the log of what we're going to capture.
     const selection = e.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
     //This variable outputs a substring. The substring takes in a few things from the 'select' event - the 
     //beginning of the selection, and the end of the selection. It then assigns those things to selection variable.
@@ -38,29 +38,40 @@ const pressed = []; // accumulator
 const secretCode = 'adventure'; //this is the code we're checking for.
 
 window.addEventListener('keyup', (e) => { //adding an event listener to the window itself.
-  console.log(e.key); //we're logging every key pressed to the console.
-  pressed.push(e.key); //we're pushing every key pressed to the accumulator 'pressed'.
-  pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
-  //we have to check the array from the back going forward. We first get the length of secretcode and specify that we're starting from the back and moving forward. Starting from the back, we use
-  //secretcode.length - 1 and then use a - at the beginning to specify that we're starting from the back. To check it against Pressed, we need to use pressed.length - secretcode.length.
+    console.log(e.key); //we're logging every key pressed to the console.
+    pressed.push(e.key); //we're pushing every key pressed to the accumulator 'pressed'.
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    //we have to check the array from the back going forward. We first get the length of secretcode and specify that we're starting from the back and moving forward. Starting from the back, we use
+    //secretcode.length - 1 and then use a - at the beginning to specify that we're starting from the back. To check it against Pressed, we need to use pressed.length - secretcode.length.
 
-  if (pressed.join('').includes(
-      secretCode
-      )) { //we use .join() to turn the pressed array back into a string for the purpose of checking it against secretCode.
-    alert('DING DING DING! YOU DID IT!'); //if it worked and we typed the code correctly, it'll do the thing.
-    // cornify_add(); //unicorns and rainbows will be added if cornify is referenced in the script!
-  }
-  console.log(pressed); //this will log the data accumulated into the array.
+    if (pressed.join('').includes(
+            secretCode
+        )) { //we use .join() to turn the pressed array back into a string for the purpose of checking it against secretCode.
+        alert('DING DING DING! YOU DID IT!'); //if it worked and we typed the code correctly, it'll do the thing.
+        cornify_add(); //unicorns and rainbows will be added if cornify is referenced in the script!
+    }
+    console.log(pressed); //this will log the data accumulated into the array.
 });
 
 
 // 4. scroll 
 
+const homeBG = document.querySelector('.home')
+const introImg = document.querySelector('.introImg')
+const introDistance = introImg.offsetTop + 'px'
 
+function logScroll(e) {
+    // console.log(window.scrollY)
+    if(window.scrollY >= introImg.offsetTop) {
+        homeBG.classList.add('blue');
+    } else {
+        homeBG.classList.remove('blue')
+    }
+}
+
+window.addEventListener('scroll', logScroll)
 
 // 5. mouseover 
 
-
-
-
+// On mouseover, I'm going to make an image disappear.
 
